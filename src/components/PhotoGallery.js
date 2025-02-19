0e2709ba80bb818f3518e6e678e9491677bb7784
Praveena Prakash Gunaga <gunagapraveena@gmail.com>
2025-02-19 23:59:25 +0530

    add chnages

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const mediaItems = [
   { type: "image", src: "./images/pic1.jpg", alt: "Temple Image 1" },
@@ -15,8 +15,20 @@ const mediaItems = [
 ];
 
 const PhotoGallery = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
+  const handleImageClick = (src) => {
+    setCurrentImage(src);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div style={{ textAlign: "center", padding: "20px", color: "#fff", background: "black" }}>
+    <div style={{ textAlign: "center", padding: "20px", color: "#000" }}>
       <h1 style={{ fontSize: "2rem", marginBottom: "20px" }}>All Temple Photos</h1>
       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
         {mediaItems.map((item, index) => (
@@ -24,17 +36,47 @@ const PhotoGallery = () => {
             key={index}
             src={item.src}
             alt={item.alt}
+            onClick={() => handleImageClick(item.src)}
             style={{
               width: "250px",
-              height: "200px",
+              height: "350px",
               objectFit: "cover",
               margin: "10px",
-              borderRadius: "10px",
-              border: "3px solid #fff",
+             
+              cursor: "pointer",
             }}
           />
         ))}
       </div>
+      {isOpen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1000,
+          }}
+          onClick={handleClose}
+        >
+          <img
+            src={currentImage}
+            alt="Expanded view"
+            style={{
+              maxHeight: "90vh",
+              maxWidth: "90vw",
+              objectFit: "contain",
+              cursor: "pointer",
+            }}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/VideoGallery.js
+++ src/components/VideoGallery.js
@@ -4,11 +4,13 @@ const mediaItems = [
   { type: "video", src: "./images/video1.mp4", alt: "Temple Video 1" },
   { type: "video", src: "./images/video2.mp4", alt: "Temple Video 2" },
   { type: "video", src: "./images/video3.mp4", alt: "Temple Video 3" },
+  { type: "video", src: "./images/video4.mp4", alt: "Temple Video 3" },
+  { type: "video", src: "./images/video5.mp4", alt: "Temple Video 3" },
 ];
 
 const VideoGallery = () => {
   return (
-    <div style={{ textAlign: "center", padding: "20px", color: "#fff", background: "black" }}>
+    <div style={{ textAlign: "center", padding: "20px", color: "#000" }}>
       <h1 style={{ fontSize: "2rem", marginBottom: "20px" }}>All Temple Videos</h1>
       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
         {mediaItems.map((item, index) => (
@@ -16,12 +18,11 @@ const VideoGallery = () => {
             key={index}
             controls
             style={{
-              width: "350px",
-              height: "250px",
-              objectFit: "cover",
-              margin: "10px",
-              borderRadius: "10px",
-              border: "3px solid #fff",
+              width: "300px",
+              height: "300px",
+             
+              borderRadius: "5px",
+              border: "1px solid #ddd",
             }}
           >
             <source src={item.src} type="video/mp4" />
